e36bb0ad6df5e1291c93682ab26ecc55b2b447ae
vipin030 <vipin030@gmail.com>
2018-01-18 21:42:26 +0100

    CHANGE: cutstomized serializer based on the data

--- expenseapp/serializers.py
+++ expenseapp/serializers.py
@@ -22,6 +22,24 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ExpenseSerializer(serializers.ModelSerializer):
 
+	category = serializers.SlugRelatedField(
+		queryset = Category.objects.all(),
+		slug_field = 'name'
+	);
+	user = serializers.SlugRelatedField(
+		queryset = User.objects.all(),
+		slug_field = 'username'
+	)
+
+	payment_type = serializers.SerializerMethodField()
+	status = serializers.SerializerMethodField()
+
 	class Meta:
 		model = Expense
 		fields = ('amount', 'payment_date', 'payment_type', 'payee', 'category', 'description', 'user', 'status')
+
+	def get_payment_type(self, obj):
+		return obj.get_payment_type_display()
+
+	def get_status(self, obj):
+		return obj.get_status_display()
